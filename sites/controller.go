4201b5aaf6036d44825368d64d0a4469797ef577
marek parafianowicz <marek.parafianowicz@netguru.co>
2018-07-12 00:23:59 +0200

    Use gin for routing and controllers helpers

--- sites/controller.go
+++ sites/controller.go
@@ -2,151 +2,104 @@ package sites
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type sitesMessage struct {
 	Sites []site `json:"sites_attr"`
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
+// Index renders all sites (TODO: pagination!)
+func Index(c *gin.Context) {
 
 	sites, err := allSites()
 	if err != nil {
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	sitesMessage := sitesMessage{sites}
-	json, err := json.Marshal(sitesMessage)
-	if err != nil {
-		http.Error(w, "JSON serialization error", 500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, string(json))
+	c.JSON(200, sites)
 }
 
-func Show(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	id := r.FormValue("id")
-	if id == "" {
-		http.Error(w, http.StatusText(400), http.StatusBadRequest)
-		return
-	}
-
+// Show renders single site
+func Show(c *gin.Context) {
+	id := c.Param("id")
 	site, err := findSite(id)
 
 	switch {
 	case err == sql.ErrNoRows:
-		http.NotFound(w, r)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record with this ID is not found"})
 		return
 	case err != nil:
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	json, err := json.Marshal(site)
-	if err != nil {
-		http.Error(w, "JSON serialization error", 500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, string(json))
+	c.JSON(200, site)
 }
 
-func Create(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	decoder := json.NewDecoder(r.Body)
-
-	atr := make(map[string]string)
-	err := decoder.Decode(&atr)
-	if err != nil {
-		panic(err)
-	}
-
-	site, err := createSite(atr)
-
-	if err != nil {
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-		return
-	}
-
-	json, err := json.Marshal(site)
-	if err != nil {
-		http.Error(w, "JSON serialization error", 500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, string(json))
+type atr struct {
+	URL string `json:"url" binding:"required"`
 }
 
-func Update(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "PATCH" && r.Method != "PUT" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+// Create adds new site to DB
+func Create(c *gin.Context) {
+	atr := atr{}
+
+	c.BindJSON(&atr)
+	if atr.URL == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "url is nil"})
 		return
 	}
 
-	decoder := json.NewDecoder(r.Body)
-
-	atr := make(map[string]string)
-	err := decoder.Decode(&atr)
+	site, err := createSite(atr.URL)
 	if err != nil {
-		panic(err)
-	}
-
-	site, err := updateSite(atr)
-
-	if err != nil {
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	json, err := json.Marshal(site)
-	if err != nil {
-		http.Error(w, "JSON serialization error", 500)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, string(json))
+	c.JSON(201, site)
 }
 
-func Delete(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "DELETE" {
-		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-		return
-	}
-
-	id := r.FormValue("id")
+// Update edits single site
+func Update(c *gin.Context) {
+	id := c.Param("id")
 	if id == "" {
-		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, "ID can't be nil")
+		return
+	}
+
+	atr := atr{}
+	c.BindJSON(&atr)
+	if atr.URL == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "url is nil"})
+		return
+	}
+
+	site, err := updateSite(id, atr.URL)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, site)
+}
+
+// Delete destroys row in DB
+func Delete(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, "ID can't be nil")
 		return
 	}
 
 	err := deleteSite(id)
 	if err != nil {
-		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("200 - OK"))
+	c.JSON(http.StatusOK, gin.H{"status": "OK"})
 }
